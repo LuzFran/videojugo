ba1456e6c1dad6b6d517e24fb0c4e8c09dbb4cd2
Lilith <80919205+LuzFran@users.noreply.github.com>
2024-03-22 18:47:11 -0300

    Update tpi.pas

--- tpi.pas
+++ tpi.pas
@@ -1,240 +1,309 @@
-program tpi;
+program Integrador;
+uses Crt;
 
-uses SysUtils,crt;
-//	------DECLARACION DE VARIABLES, CONSTANTES Y MATRICES------
-Const
-	T = 10; //medida de la caja con los numeros de entrada y salida
-	P = 5; //pantallas
-type
-	pantalla = record
-			num,posx,posy:integer;
-			dir:string;
-			encontrado:boolean;
+const
+	Filas	= 10;
+	Columnas= 10;
+	Espejos	= 5;
+Type
+	TTEspejos = Record
+					num,x,y,dir	:integer;
 	end;
-	JugadorData = record
-			NroDisparos, puntaje, EspejosHayados:integer;
-			Nombre: string;
+	TTJugador	= Record
+					NroDisparos,Puntuacion:	integer;
+					EspejosEncontrados:		byte;
+					Nombre:					String;
 	end;
-	pantallas = array [1..P] of pantalla;
-	cajaNegra = array [1..T,1..T] of shortstring;
+	TTPantallas = array [1..Espejos] of TTEspejos;
+	TTMatriz = array [1..Filas,1..Columnas] of shortstring;
 
-//	------PROCEDIMIENTOS DEL PROGRAMA PRINCIPAL------
-//	------	pantalla principal del juego	------
-procedure PantallaInicio();
-	//	----procedimiento para centrar los texto----
-	function CentrarTexto(texto:string):string;
-	var  ancho, i: Integer;
-	begin
-		ancho := (80 - Length(texto)) div 2; // 80 es el ancho de la pantalla
-		CentrarTexto := '';
-		for i := 1 to ancho do
-			CentrarTexto := CentrarTexto + ' ';
-		CentrarTexto := CentrarTexto + texto;
-	end;
-var	i:integer;
+//	------ INICIO PROCEDIMIENTO INICIALIZAR------
+procedure Inicializar (var M:TTMatriz; var Jugador:TTJugador);
+var i,j:integer;
 Begin
-	writeln;writeln;
+	for i:= 1 to Filas do
+		for j:= 1 to Columnas do
+			M[i,j]:= ' ';
+	Jugador.NroDisparos:= 0;
+	Jugador.EspejosEncontrados:= 0;
+	Jugador.Puntuacion:= 0;
+	Jugador.Nombre:= ' ';
+End;
+//	------  FIN PROCEDIMIENTO INICIALIZAR  ------
+//	------ INICIO FUNCION PARA CENTRAR EL TEXTO ------
+function CentrarTexto(texto:string):String;
+var i,Ancho: integer;
+begin
+	Ancho:= (80 - Length(texto)) div 2;
+	CentrarTexto:= '';
+	for i:= 1 to Ancho do
+		CentrarTexto:= CentrarTexto + ' ';
+	CentrarTexto:= CentrarTexto + texto;
+end;
+//	------   FIN FUNCION PARA CENTRAR EL TEXTO  ------
+//	------ INICIO PROCEDIMIENTO PANTALLA DE INICIO ------
+procedure PantallaInicio(var Jugador:TTJugador);
+var i:integer;
+Begin
+	writeln;
 	writeln(CentrarTexto('LASER TAG'));
-	writeln(CentrarTexto(''));
+	writeln('');
 	writeln(CentrarTexto('Trabajo practico integrador'));
 	writeln(CentrarTexto('by: Abril y Luz'));
-	writeln(CentrarTexto(''));
-	write('cargando juego: ');
-	for i:= 1 to 20 do
+	writeln('');
+	writeln(CentrarTexto('Cargando Juego... '));
+	writeln;
+	for i:= 1 to 40 do
 	begin
-		write('❚');
+		write('//');
 		delay(200);
 	end;
+	writeln;
+	writeln(CentrarTexto('Ingrese su nombre'));
+	read(Jugador.Nombre);
 	writeln;writeln;
-	writeln('press "S" for star');
+	writeln('Press "S" for strarting');
 	readkey;
-End;
+end;
+//	------   FIN PROCEDIMIENTO PANTALLA DE INICIO  ------
+//	------ INICIO PROCEDIMIENTO MENU DE OPCIONES ------
+procedure MenuOpciones(var pantalla:TTPantallas; var Jugador:TTJugador;const Espejos:integer);
 
-//	------	inicializa los datos necesarios para el juego
-procedure inicializarJuego(var pant:pantallas; jugador:JugadorData; const P:integer);
-	//	----procedimiento para posicion de pantallas random----
-	procedure ubicacion(opcion:integer; var pant:pantallas; const P:integer);
-		function conversor(n:integer):integer;
-		var x,y:integer;
-		begin
-			x:= 10;
-			y:= 9;
-			if (n >= 0) or (n <= 9) then 
-				conversor:= x - n
-			else if (n >= 10) or (n <= 19) then
-				conversor:= n - y;
-			end;
-	var i,n,x,y,dire:integer;
-	Begin
-		n:=0;
-		if opcion = 2 then
-		Begin
-			writeln;
-			writeln('Jugador 1, cargue la pposicion de las pantallas');
-			writeln;
-			for i:= 1 to P do
-			begin
-				pant[i].num:= i;
-				pant[i].encontrado:= false;
-				writeln('ingrese la coordena de 0 a 9:');
-				readln(x);
-				pant[i].posX:= conversor(x);
-				writeln('ingrese la coordena de 10 a 19:');
-				readln(y);
-				pant[i].posY:= conversor(y);
-				writeln('ingrese la direccion: ');
-				writeln('1. derecha');
-				writeln('2.izquierda');
-				readln(dire);
-				if (dire = 1) then pant[i].dir:= '/'
-				else if (n = 2) then pant[i].dir:= '\';
-			end;
-		end
-		else if opcion = 2 then
-		Begin
-			for i:= 2 to P do
-			begin
-				pant[i].num:= i;
-				pant[i].encontrado:= false;
-				n:= random(11) + 1;
-				if (n = 0) then pant[i].posX:= (n + 1)
-				else pant[i].posX:= n;
-				n:= random(11) + 1;
-				if (n = 0) then pant[i].posY:= (n + 1)
-				else pant[i].posY:= n;
-				n:=0;
-				n:= random(2);
-				if (n = 0) then pant[i].dir:= '/'
-				else if (n = 1) then pant[i].dir:= '\';
-			end;
-		End;
-	end;	
-var respuesta: byte;
+var opciones:byte;
+	i,x,y:integer;
 Begin
 	writeln;
-	writeln('¿cuantos jugadores?');
-	writeln('1 o 2');
-	writeln;
-	readln(respuesta);
-	ubicacion(respuesta,pant,P);
-	clrscr;
-	writeln;
-	writeln('Nivel');
-	writeln('1. 20 disparos');
-	writeln('2. 10 disparos');
-	writeln;
-	readln(respuesta);
-	case respuesta of
-			1 : jugador.NroDisparos:= 20;
-			2 : jugador.NroDisparos:= 10;
-	end;
-	writeln;
-	write('ingrese el nombre del jugador ');
-	read(jugador.Nombre);
-	writeln;
-end;
+	writeln(CentrarTexto('Ingrese el Nivel'));
+	writeln(CentrarTexto('1. Nivel 1   2. Nivel 2'));
+	ReadLn(opciones);
 
-//	------juego------
-procedure juego(var cn:cajaNegra ;var pant:pantallas; var jugador:JugadorData; const P,T:integer);
-	Procedure MostrarMatriz (Var cn:cajaNegra; const T:integer);
-	Var i,j:Integer;
+	case opciones of
+				1: begin
+					writeln;
+					writeln(CentrarTexto('el nivel 1 contiene 20 disparos'));
+					Jugador.NroDisparos:= 20;
+					delay(3000);
+					end;
+				2: begin
+					writeln;
+					writeln(CentrarTexto('el nivel 2 contiene 10 disparos'));
+					Jugador.NroDisparos:= 10;
+					delay(3000);
+					end;
+	end;
+
+	writeln;
+	writeln(CentrarTexto('Ingrese cuantos jugadores'));
+	writeln(CentrarTexto('1. 1 jugador (la posicion de los espejos son aleatorios)'));
+	writeln(CentrarTexto('2. 2 jugadores (la posicion de los espejos son puestas)'));
+	writeln(CentrarTexto('por el segundo jugador'));
+	ReadLn(opciones);
+
+	case opciones of
+				1: begin
+					for i:= 1 to Espejos do
+					begin
+						x:= random(11) + 1;
+						if (x = 0) then pantalla[i].x:= (x + 1)
+						else pantalla[i].x:= x;
+						y:= random(11) + 1;
+						if (y = 0) then pantalla[i].y:= (y + 1)
+						else pantalla[i].y:= y;
+						x:=0;
+						x:= random(3);
+						if (x = 0) then pantalla[i].dir:= (x + 1) //'/'
+						else pantalla[i].dir:= x; //'\';
+					end;
+					end;
+				2: begin
+					for i:= 1 to Espejos do
+					begin
+						pantalla[i].num:= i;
+						writeln;
+						writeln(CentrarTexto('ingrese la posicion entre 0 y 9'));
+						ReadLn(y);
+						pantalla[i].y:= 10 - y;
+						writeln(CentrarTexto('ingrese la posicion entre 10 y 19'));
+						ReadLn(x);
+						pantalla[i].x:= x - 9;
+						writeln(CentrarTexto('ingrese la direccion del espejo'));
+						writeln(CentrarTexto('1. derecha  2. izquierda'));
+						ReadLn(opciones);
+						if opciones = 1 then pantalla[i].dir:= opciones //'/'
+						else if opciones = 2 then pantalla[i].dir:= opciones;//'\'
+					end;
+					end;
+	end;
+end;
+//	------   FIN PROCEDIMIENTO MENU DE OPCIONES  ------
+//	------ INICIO PROCEDIMIENTO JUEGO ------
+{procedure juego(Pantalla:TTPantallas; var Jugador:TTJugador; const Espejos:integer;var Matriz:TTMatriz);
+	Procedure MostrarMatriz (Var M:TTMatriz;Var JJ:TTJugador);
+	Var	i,j:Integer;
 	Begin
 		WriteLn;
+		WriteLn('Bienvenido! ',JJ.Nombre);
+		WriteLn;
+		Write('   | Puntos:',JJ.Puntuacion,'| Nro de Disparos:',JJ.NroDisparos,'| Espejos Encontrados:',JJ.EspejosEncontrados,'|');
+		WriteLn;
+		WriteLn;
 		WriteLn('   10 11 12 13 14 15 16 17 18 19  ');
-		WriteLn('    __ __ __ __ __ __ __ __ __ __  ');
-		For i:= 1 to T do
-		Begin
+		WriteLn('   __ __ __ __ __ __ __ __ __ __   ');
+		For i:= 1 to Filas do
+	    Begin
 			Write(10 - i,' ');
 			Write('|');
-			For j:=1 to T do
+			For j:=1 to Columnas do
 			Begin
-				Write(cn[i,j]:2);
+				Write(M[i,j]:2);
 				Write(' ');
 			End;
 			Write('|');
 			Write(' ', 19 + i);
 			WriteLn;
 		End;
-		WriteLn('   __ __ __ __ __ __ __ __ __ __  ');
+		WriteLn('   __ __ __ __ __ __ __ __ __ __   ');
 		WriteLn('   39 38 37 36 35 34 33 32 31 30  ');
-	End;
-	function MenuOpciones():byte;
-	begin
-		writeln('(1).disparar  (2).adivinar  (3).salir');
-		ReadLn(MenuOpciones);
-	end;
-	procedure adivinar(var pant:pantallas; var cn:cajaNegra; var jugador:JugadorData; const T,P:integer);
-		procedure comprobador(var x,y: integer;dire:String; var pant:pantallas; const P:integer);
-		var i,auxX,auxY:integer;
-			direaux:String;
-		Begin
-			x:= 10 - x;
-			y:= y - 9;
-			if dire = '1' then dire := '/'
-			else if dire = '2' then dire := '\';
-
-			for i:= 1 to 5 do
-			begin
-				auxX:= pant[i].posX;
-				auxY:= pant[i].posY;
-				direaux := pant[i].dir;
-				
-				if (x = auxX) and (y = auxY) and (direaux = dire) then
-				Begin
-					pant[i].encontrado:= True; 
-					WriteLn('¡FELICIDADES! La Opcion ingresada es Correcta ');
-					Jugador.Puntaje := Jugador.Puntaje + 2;
-					CN[pant[i].posX,pant[i].posY] := pant[i].dir;
-				end
-				else writeln('¡PERDISTE! La opcion ingresada no es la Correcta');
-			end;
-		end;
-	
-	
-	var x,y:integer;
-		dire:String;
-	begin
-		WriteLn('¡ADVERTENCIA!');
-		WriteLn('Solo posee un intento...');
 		WriteLn;
-		WriteLn('ingrese la coordenada donde cree que esta la pantalla');
-		WriteLn('entre 0 y 9');
-		readln(x);
-		WriteLn('ingrese la coordenada donde cree que esta la pantalla');
-		WriteLn('entre 10 y 19');
-		readln(y);
-		WriteLn('ingrese la direccion que tiene la pantalla');
-		WriteLn('1.derecha(/)  2.izquierda(\)');
-		readln(dire);
-		comprobador(x,y,dire,pant,P);
-	end;
-	
-	
-var //entrada, respuesta:integer;
-	opcion:byte;
+		writeln('(1).Disparar  (2).Estimar Espejos  (3).Salir');
+	End;
+var i,x,y,d,halladas:  integer;
 Begin
-	opcion:= 0;
-	MostrarMatriz(cn,T);
-	opcion:= MenuOpciones;
-	if opcion = 1 then
-		disparar(pant,cn,jugador,T,P)
-	else if opcion = 2 then
-		adivinar (pant,cn,jugador,T,P);
-end;
-//	------PROGRAMA PRINCIPAL------
-var cn:		cajaNegra;
-	pant:	pantallas;
-	jugador:JugadorData;
+	halladas:= 0;
+	while (halladas <> 5) do
+	begin
+		mostrarMatriz(Matriz,Jugador);
+		writeln('ingrese la posicion que cree que esta el espejo entre 0 y 9');
+		ReadLn(y);
+		y:= 10 - y;
+		writeln('ingrese la posicion que cree que esta el espejo entre 10 y 19');
+		ReadLn(x);
+		x:= x - 9;
+		writeln('ingrese la direccion que cree que esta el espejo');
+		writeln('1. derecha  2. izquierda');
+		ReadLn(d);
+		for i:= 1 to Espejos do
+		begin
+			if (x = Pantalla[i].x) and (y = Pantalla[i].y) and (d = Pantalla[i].dir) then
+			begin
+				Jugador.EspejosEncontrados+= 1;
+				writeln('¡FELICIDADES! Encontraste el espejo nro ', i);
+				Jugador.Puntuacion += 2;
+				if (d = 1) then Matriz[y,x]:= '/'
+				else if (d = 2) then Matriz[y,x]:= '\';
+				halladas+=1;
+			end
+			else writeln('¡FALLASTE! los datos ingresados son incorrectos');
+		end;
+	end;
+end;}
+//	------   FIN PROCEDIMIENTO JUEGO  ------
+//	------ INICIO PROCEDIMIENTO JUEGO------
+procedure juego(Pantalla:TTPantallas; var Jugador:TTJugador; const Espejos:integer;var Matriz:TTMatriz);
+	Procedure MostrarMatriz (Var M:TTMatriz;Var JJ:TTJugador);
+	Var	i,j:Integer;
+	Begin
+		WriteLn;
+		WriteLn('Bienvenido! ',JJ.Nombre);
+		WriteLn;
+		Write('   | Puntos:',JJ.Puntuacion,'| Nro de Disparos:',JJ.NroDisparos,'| Espejos Encontrados:',JJ.EspejosEncontrados,'|');
+		WriteLn;
+		WriteLn;
+		WriteLn('   10 11 12 13 14 15 16 17 18 19  ');
+		WriteLn('   __ __ __ __ __ __ __ __ __ __   ');
+		For i:= 1 to Filas do
+	    Begin
+			Write(10 - i,' ');
+			Write('|');
+			For j:=1 to Columnas do
+			Begin
+				Write(M[i,j]:2);
+				Write(' ');
+			End;
+			Write('|');
+			Write(' ', 19 + i);
+			WriteLn;
+		End;
+		WriteLn('   __ __ __ __ __ __ __ __ __ __   ');
+		WriteLn('   39 38 37 36 35 34 33 32 31 30  ');
+		WriteLn;
+	End;
+	procedure Estimar(Pantalla:TTPantallas; var Jugador:TTJugador; const Espejos:integer);
+	var i,x,y,d,halladas:integer;
+		estado:boolean;
+	Begin
+		halladas:= 0;
+		estado:= false;
+		begin
+			writeln('ingrese la posicion que cree que esta el espejo entre 0 y 9');
+			ReadLn(y);
+			y:= 10 - y;
+			writeln('ingrese la posicion que cree que esta el espejo entre 10 y 19');
+			ReadLn(x);
+			x:= x - 9;
+			writeln('ingrese la direccion que cree que esta el espejo');
+			writeln('1. derecha  2. izquierda');
+			ReadLn(d);
+			for i:= 1 to Espejos do
+			begin
+				if (x = Pantalla[i].x) and (y = Pantalla[i].y) and (d = Pantalla[i].dir) then
+				begin
+					Jugador.EspejosEncontrados+= 1;
+					writeln('¡FELICIDADES! Encontraste el espejo nro ', i);
+					Jugador.Puntuacion += 2;
+					if (d = 1) then Matriz[y,x]:= '/'
+					else if (d = 2) then Matriz[y,x]:= '\';
+					halladas+=1;
+					estado:= true;
+				end
+				else estado:= false;
+			end;
+			if estado = false then writeln('¡FALLASTE! los datos ingresados son incorrectos');
+		end;
+		clrscr;
+	end;
+Procedure Disparar (Var Pantalla:TTPantallas;Var Jugador:TTJugador;Const:Espejos:Integer);
+Var
+Entrada:Integer;
+AuxEntrada:Integer;
+Salida:Integer;
+i,j:Integer;
+Begin;
+WriteLn('Ingrese por donde desea disparar el lazer..(0..39)')
+ReadLn(Entrada);
+End; 	
+	
+	
+var opciones:integer;
+Begin
+	opciones:= 0;
+	while (opciones <> 3) do
+	begin
+		MostrarMatriz(Matriz,Jugador);
+		writeln('(1).Disparar  (2).Estimar Espejos  (3).Salir');
+		ReadLn(opciones);
+		case opciones of
+				1: Estimar(Pantalla,Jugador,Espejos);//Disparar(Pantalla,Espejos);
+				2: Estimar(Pantalla,Jugador,Espejos);
+		end;
+	end;
+End;
+//	------ FIN PROCEDIMIENTO JUEGO ------
+//	------ INICIO ------
+
+//	------ FIN ------
+//	------ INICIO ------
+
+//	------ FIN ------
+var 
+	MMatriz:TTMatriz;
+	JJugador:TTJugador;
+	PPantallas:TTPantallas;
 BEGIN
 	Randomize();
-	Jugador.NroDisparos:= 0;
-	Jugador.puntaje:= 0;
-	Jugador.EspejosHayados:= 0;
-	Jugador.NroDisparos:= 0;
-	Jugador.Nombre:= ' ';
-	PantallaInicio();
+	Inicializar(MMatriz,JJugador);
+	PantallaInicio(JJugador);
 	clrscr;
-    inicializarJuego(pant,jugador,P);
-	clrscr;
-	juego(cn,pant,jugador,P,T);
+	MenuOpciones(PPantallas,JJugador,Espejos);
+	juego(PPantallas,JJugador,Espejos,MMatriz);
 END.
